6200986f67ebd06db717c929cc9911668510c94f
JeremyGueCo <jeremy.guerincoquereau@gmail.com>
2019-07-17 08:10:25 +0200

    MAJ Style

--- wildCircusUI/src/app/features/artist-admin/artist-admin.component.ts
+++ wildCircusUI/src/app/features/artist-admin/artist-admin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ArtistService } from 'src/app/shared/services/artist.service';
 import { Artist } from 'src/app/shared/models/artist.model';
+import { error } from 'util';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-artist-admin',
@@ -9,13 +11,46 @@ import { Artist } from 'src/app/shared/models/artist.model';
 })
 export class ArtistAdminComponent implements OnInit {
 
+  public update: boolean = false;
   public artists: Artist[] = [];
+  public isCreate: boolean = false;
+
   constructor(
     public artistService: ArtistService,
+    private modalService: NgbModal
   ) { }
 
   ngOnInit() {
     this.artistService.getAllArtists().subscribe(allArtist => this.artists = allArtist)
   }
 
+  updateArtist() {
+    this.update = !this.update;
+  }
+
+  modifyArtist(Artist: Artist) {
+    this.artistService.updateArtistByID(Artist.id, Artist).subscribe();
+    this.update = !this.update;
+  }
+
+  cancelArtist(){
+    this.isCreate = !this.isCreate
+  }
+
+  deleteArtist(id, index) {
+    this.artistService.deleteArtistByID(id).subscribe();
+    this.artists.splice(index, 1)
+  }
+
+  createArtist(artist: Artist){
+    this.artistService.createArtist(artist).subscribe((newArtist) => this.artists.push(newArtist))
+  }
+
+  create(){
+    this.isCreate = true
+  }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
+  }
 }
--- wildCircusUI/src/app/shared/services/artist.service.ts
+++ wildCircusUI/src/app/shared/services/artist.service.ts
@@ -16,11 +16,25 @@ export class ArtistService {
   public api = `${environment.apiUrl}`;
 
 
-  getAllArtists(): Observable<Artist[]>{
+  getAllArtists(): Observable<Artist[]> {
     return this.http.get<Artist[]>(`${this.api}/artists`);
   }
 
-  getArtistsByShow(id): Observable<Artist[]>{
+  getArtistsByShow(id): Observable<Artist[]> {
     return this.http.get<Artist[]>(`${this.api}/shows/${id}/artists`)
   }
+
+  updateArtistByID(id, formData): Observable<Artist> {
+    return this.http.put<Artist>(`${this.api}/artists/${id}`, formData)
+  }
+
+  deleteArtistByID(id): Observable<Artist> {
+    return this.http.delete<Artist>(`${this.api}/artists/${id}`)
+  }
+
+  createArtist(formData): Observable<Artist> {
+    return this.http.post<Artist>(`${this.api}/artists`, formData)
+  }
+
+
 }
