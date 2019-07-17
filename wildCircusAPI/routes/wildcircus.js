9cfa5e46c01294ec31bb505ddfc1b961d3fa5af9
JeremyGueCo <jeremy.guerincoquereau@gmail.com>
2019-07-17 09:11:16 +0200

    MAJ Json

--- wildCircusAPI/routes/wildcircus.js
+++ wildCircusAPI/routes/wildcircus.js
@@ -16,19 +16,19 @@ router.post('/bookings', (req, res) => {
    const formData = req.body;
    db.query('INSERT INTO bookings SET ?', formData, (err, results) => {
       if (err) {
-         res.status(500).send("Erreur lors de la sauvegarde d'une nouvelle réservation");
+         res.status(500).json("Erreur lors de la sauvegarde d'une nouvelle réservation");
          return
       }
       if (!results) {
-         res.status(400).send();
+         res.status(400).json('Aucun résultat');
          return;
       }
       db.query('SELECT * FROM bookings WHERE id = ?', results.insertId, (err, results) => {
          if (err) {
-            res.status(500).send();
+            res.status(500).json('Erreur dans la récupération');
             return;
          }
-         res.status(201).send(results[0]);
+         res.status(201).json(results[0]);
       });
    });
 });
@@ -39,19 +39,19 @@ router.post('/messages', (req, res) => {
    const formData = req.body;
    db.query('INSERT INTO messages SET ?', formData, (err, results) => {
       if (err) {
-         res.status(500).send("Erreur lors de l'enregistrement d'un nouveau message");
+         res.status(500).json("Erreur lors de l'enregistrement d'un nouveau message");
          return
       }
       if (!results) {
-         res.status(400).send();
+         res.status(400).json('Aucun résultat');
          return;
       }
       db.query('SELECT * FROM messages WHERE id = ?', results.insertId, (err, results) => {
          if (err) {
-            res.status(500).send();
+            res.status(500).json();
             return;
          }
-         res.status(201).send(results[0]);
+         res.status(201).json(results[0]);
       });
    });
 });
@@ -62,11 +62,11 @@ router.get('/artists', (req, res) => {
    const idArtist = req.params.id;
    db.query('SELECT * FROM artists', idArtist, (err, results) => {
       if (err) {
-         res.status(500).send("Erreur lors de la récupération des artistes");
+         res.status(500).json("Erreur lors de la récupération des artistes");
          return;
       }
       if (!results.length) {
-         res.status(404).send("Aucun résultat");
+         res.status(404).json("Aucun résultat");
          return;
       }
       res.status(200).json(results);
@@ -79,19 +79,19 @@ router.post('/artists', (req, res) => {
    const formData = req.body;
    db.query('INSERT INTO artists SET ?', formData, (err, results) => {
       if (err) {
-         res.status(500).send("Erreur lors de l'enregistrement d'un nouvel artiste");
+         res.status(500).json("Erreur lors de l'enregistrement d'un nouvel artiste");
          return
       }
       if (!results) {
-         res.status(400).send();
+         res.status(400).json();
          return;
       }
       db.query('SELECT * FROM artists WHERE id = ?', results.insertId, (err, results) => {
          if (err) {
-            res.status(500).send();
+            res.status(500).json('Erreur lors de la récupération de l\'artiste créé');
             return;
          }
-         res.status(201).send(results[0]);
+         res.status(201).json(results[0]);
       });
    });
 });
@@ -102,15 +102,15 @@ router.put('/artists/:id', (req, res) => {
    const formData = req.body;
    db.query('UPDATE artists SET ? WHERE id = ?', [formData, id], (err, results) => {
       if (err) {
-         res.status(500).send("Erreur lors de la modification d'un artiste");
+         res.status(500).json("Erreur lors de la modification d'un artiste");
          return;
       }
       db.query('SELECT * FROM artists WHERE id = ?', id, (err, results) => {
          if (err) {
-            res.status(500).send();
+            res.status(500).json('Erreur lors de la récupération de l\'artiste modifié');
             return;
          }
-         res.status(200).send(results[0]);
+         res.status(200).json(results[0]);
       });
    });
 });
@@ -119,10 +119,10 @@ router.delete('/artists/:id', (req, res) => {
    const id = req.params.id;
    db.query('DELETE FROM artists WHERE id = ? ', id, err => {
       if (err) {
-         res.status(500).send("Erreur lors de la suppression d'un artiste");
+         res.status(500).json("Erreur lors de la suppression d'un artiste");
          return;
       }
-      res.status(201).send()
+      res.status(201).json()
    })
 })
 
@@ -133,11 +133,11 @@ router.get('/bookings', (req, res) => {
    const idBooking = req.params.id;
    db.query('SELECT * FROM bookings', idBooking, (err, results) => {
       if (err) {
-         res.status(500).send("Erreur lors de la récupération des réservations");
+         res.status(500).json("Erreur lors de la récupération des réservations");
          return;
       }
       if (!results.length) {
-         res.status(404).send("Aucun résultat");
+         res.status(404).json("Aucun résultat");
          return;
       }
       res.status(200).json(results);
@@ -149,11 +149,11 @@ router.get('/bookings', (req, res) => {
 router.get('/messages', (req, res) => {
    db.query('SELECT * FROM messages', (err, results) => {
       if (err) {
-         res.status(500).send("Erreur lors de la récupération des messages");
+         res.status(500).json("Erreur lors de la récupération des messages");
          return;
       }
       if (!results.length) {
-         res.status(404).send("Aucun résultat");
+         res.status(404).json("Aucun résultat");
          return;
       }
       res.status(200).json(results);
@@ -165,19 +165,19 @@ router.post('/shows', (req, res) => {
    const formData = req.body;
    db.query('INSERT INTO shows SET ?', formData, (err, results) => {
       if (err) {
-         res.status(500).send("Erreur lors de l'enregistrement d'un nouveau spectacle");
+         res.status(500).json("Erreur lors de l'enregistrement d'un nouveau spectacle");
          return
       }
       if (!results) {
-         res.status(400).send();
+         res.status(400).json();
          return;
       }
       db.query('SELECT * FROM shows WHERE id = ?', results.insertId, (err, results) => {
          if (err) {
-            res.status(500).send();
+            res.status(500).json('Erreur lors de la récupération du spectacle créé');
             return;
          }
-         res.status(201).send(results[0]);
+         res.status(201).json(results[0]);
       });
    });
 });
@@ -188,11 +188,11 @@ router.get('/shows', (req, res) => {
    const idShow = req.params.id;
    db.query('SELECT * FROM shows', idShow, (err, results) => {
       if (err) {
-         res.status(500).send("Erreur lors de la récupération des spectacles");
+         res.status(500).json("Erreur lors de la récupération des spectacles");
          return;
       }
       if (!results.length) {
-         res.status(404).send("Aucun résultat");
+         res.status(404).json("Aucun résultat");
          return;
       }
       res.status(200).json(results);
@@ -205,11 +205,11 @@ router.get('/shows/:id/artists', (req, res) => {
    const idShow = req.params.id;
    db.query('SELECT a.name, a.picture, a.skills FROM artists a JOIN show_artist sa ON sa.artist_id = a.id JOIN shows s on s.id = sa.show_id WHERE s.id = ?', idShow, (err, results) => {
       if (err) {
-         res.status(500).send("Erreur lors de la récupération des artistes d'un spectacle");
+         res.status(500).json("Erreur lors de la récupération des artistes d'un spectacle");
          return;
       }
       if (!results.length) {
-         res.status(404).send("Aucun résultat");
+         res.status(404).json("Aucun résultat");
          return;
       }
       res.status(200).json(results);
@@ -220,11 +220,11 @@ router.get('/shows/:id', (req, res) => {
    const idShow = req.params.id;
    db.query('SELECT * FROM shows  WHERE id = ?', idShow, (err, results) => {
       if (err) {
-         res.status(500).send("Erreur lors de la récupération des spectacles");
+         res.status(500).json("Erreur lors de la récupération des spectacles");
          return;
       }
       if (!results.length) {
-         res.status(404).send("Aucun résultat");
+         res.status(404).json("Aucun résultat");
          return;
       }
       res.status(200).json(results);
@@ -235,11 +235,11 @@ router.get('/shows/:id/bookings', (req, res) => {
    const idShow = req.params.id;
    db.query('SELECT * FROM bookings WHERE show_id = ?', idShow, (err, results) => {
       if (err) {
-         res.status(500).send("Erreur lors de la récupération des réservations");
+         res.status(500).json("Erreur lors de la récupération des réservations");
          return;
       }
       if (!results.length) {
-         res.status(404).send("Aucun résultat");
+         res.status(404).json("Aucun résultat");
          return;
       }
       res.status(200).json(results);
